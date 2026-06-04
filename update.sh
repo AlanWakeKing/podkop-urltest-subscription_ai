524cc8ea9c72cb03bd875d38b3d5554ac6f681e6
Aleksey <lexamenz@ya.ru>
2026-06-04 22:49:41 +0300

    Обновлена версия скрипта до 2026.06.04

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@
 # Здесь можно указать:
 # - raw-ссылки на подписки
 # - прямые ссылки вида vless://, ss://, trojan://, socks://, hy2://
-SCRIPT_VERSION="2026.05.11"
+SCRIPT_VERSION="2026.06.04"
 SOURCE_1=""
 SOURCE_2=""
 SOURCE_3=""
